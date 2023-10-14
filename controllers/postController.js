6edb8e458c535324694a22397a367e9a8bef23c8
Liubov <liubovusikova1@gmail.com>
2023-10-14 20:59:35 +0300

    add posts methods

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,90 @@
-export const create = (req, res) => {
+import PostModel from "../models/Post.js";
+export const getAll = async (req, res) => {
 	try {
-	} catch (err) {}
+		const posts = await PostModel.find().populate("user").exec();
+		res.json(posts);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Failed to get posts",
+		});
+	}
+};
+
+export const getOne = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		PostModel.findOneAndUpdate(
+			{ _id: postId },
+			{ $inc: { viewsCount: 1 } },
+			{ returnDocument: "after" },
+			(err, doc) => {
+				if (err) {
+					return res.status(500).json({
+						message: "Failed to return post",
+					});
+					if (!doc) {
+						return res.status(404).json({
+							message: "Failed to find post",
+						});
+					}
+					res.json(doc);
+				}
+			},
+		);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Failed to get posts",
+		});
+	}
+};
+
+export const remove = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		PostModel.findOneAndDelete(
+			{
+				_id: postId,
+			},
+			(err, doc) => {
+				if (err) {
+					return res.status(500).json({
+						message: "Failed to delete post",
+					});
+				}
+				if (!doc) {
+					return res.status(404).json({
+						message: "Failed to find post",
+					});
+				}
+				res.json({
+					success: true,
+				});
+			},
+		);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Failed to get posts",
+		});
+	}
+};
+
+export const create = async (req, res) => {
+	try {
+		const doc = new PostModel({
+			title: req.body.title,
+			text: req.body.text,
+			imageUrl: req.body.imageUrl,
+			user: req.userId,
+		});
+		const post = await doc.save();
+		res.json(post);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Failed to create post",
+		});
+	}
 };
